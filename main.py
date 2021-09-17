49aed2415fd020ef0fe19d90b22749b1f19d92f4
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:29:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+phrase = 'In space, no one can hear you code.'
+my_list = ['B', 'n', 'n', '5']
+cargo_hold = "water,space suits,food,plasma sword,batteries"
+
+# The split and list methods convert a string into a list, while the join method does the opposite.
+
+# See what happens when you print phrase.split() vs. phrase.split('e') vs. list(phrase). What is the purpose of the argument inside the ()?
+
+
+# See what happens when you print ''.join(my_list) vs. 'a'.join(my_list) vs. '_'.join(my_list). What is the purpose of the argument inside the ()?
+
+
+# Split the cargo_hold string at each comma, alphabetize the list with sort, then combine the elements into a new string. Use a hyphen to join the elements together in the string.
+
+
+# Do split, list, or join change the original string/list?
